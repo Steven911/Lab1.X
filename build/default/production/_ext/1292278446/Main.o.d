72805b28c5f213d8ae88d7e9e2d6488dac1783d4
Steven Velandia <Steven Velandia@STEVEN-PC>
2017-03-13 16:16:17 -0500

    DOS

---
+++ build/default/production/_ext/1292278446/Main.o.d
@@ -0,0 +1,9 @@
+build/default/production/_ext/1292278446/Main.o:  \
+	D:/Electronic\ Engineering/uC\ Marlon/Labs/Lab1.X/Main.asm  \
+	D:/Electronic\ Engineering/uC\ Marlon/Labs/Lab1.X/CONFIGURACIONES.inc  \
+	D:/Electronic\ Engineering/uC\ Marlon/Labs/Lab1.X/CONVERSOR.inc  \
+	D:/Electronic\ Engineering/uC\ Marlon/Labs/Lab1.X/SECUENCIA.inc  \
+	D:/Electronic\ Engineering/uC\ Marlon/Labs/Lab1.X/p16f887.inc  \
+	D:/Electronic\ Engineering/uC\ Marlon/Labs/Lab1.X/RETARDO.inc  \
+	D:/Electronic\ Engineering/uC\ Marlon/Labs/Lab1.X/VER.inc  \
+
